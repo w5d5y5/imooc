668bb00aedca986bb76b894aa189ca3da84beffc
wdy <w5d5y5@163.com>
2020-05-13 12:13:31 +0800

    框架1

---
+++ src/main/java/com/imooc/springboot/study/imoocspringbootstudy/ImoocSpringbootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.springboot.study.imoocspringbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocSpringbootStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/springboot/study/imoocspringbootstudy/ImoocSpringbootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.springboot.study.imoocspringbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocSpringbootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
